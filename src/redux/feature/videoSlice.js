5c349e35047638fb3cacbea71dc23c4f14e643d3
akram-aqad <akram.khan@aqad.ae>
2025-06-13 09:10:03 +0530

    video api error handle

--- src/redux/feature/videoSlice.js
+++ src/redux/feature/videoSlice.js
@@ -1,25 +1,30 @@
-import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import RNFS from 'react-native-fs';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Toast from "react-native-toast-message";
 
 export const fetchVideos = createAsyncThunk(
-  'videos/fetch',
+  "videos/fetch",
   async (_, thunkAPI) => {
     try {
       const res = await axios.get(
-        'https://gist.githubusercontent.com/poudyalanil/ca84582cbeb4fc123a13290a586da925/raw/14a27bd0bcd0cd323b35ad79cf3b493dddf6216b/videos.json',
+        "https://gist.githubusercontent.com/poudyalanil/ca84582cbeb4fc123a13290a586da925/raw/14a27bd0bcd0cd323b35ad79cf3b493dddf6216b/videos.json"
       );
-      await AsyncStorage.setItem('allVideoData', JSON.stringify(res.data));
+      await AsyncStorage.setItem("allVideoData", JSON.stringify(res.data));
       return res.data; // Ensure videos key is used
     } catch (error) {
-      return thunkAPI.rejectWithValue('Failed to fetch videos.');
+      Toast.show({
+        type: "error",
+        text1: error.message,
+        text2: error.message,
+      });
+      return thunkAPI.rejectWithValue("Failed to fetch videos.");
     }
-  },
+  }
 );
 
 const videoSlice = createSlice({
-  name: 'videos',
+  name: "videos",
   initialState: {
     items: [],
     loading: false,
@@ -27,9 +32,9 @@ const videoSlice = createSlice({
     download: {}, // { id: path }
   },
   reducers: {},
-  extraReducers: builder => {
+  extraReducers: (builder) => {
     builder
-      .addCase(fetchVideos.pending, state => {
+      .addCase(fetchVideos.pending, (state) => {
         state.loading = true;
         state.error = null;
       })
